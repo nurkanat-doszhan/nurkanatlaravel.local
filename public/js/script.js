3e63fc0fce3ac19bb71dfd6d6e54bf8b30469867
курносове <123456>
2016-06-21 21:04:44 +0600

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+    var taskDelete = $('.delete');
+
+    taskDelete.submit(function(){
+        var task = $(this).find('input').attr('value');
+        var self = $(this);
+        $.ajax({
+            url: '/newTask/' + task,
+            type: 'delete',
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success:function(data){
+                console.log(self.parents('tr').fadeOut(600));
+            },
+            error: function(err){
+                console.log(err.responseText);
+            }
+        });
+
+        return false;
+    })
+});
